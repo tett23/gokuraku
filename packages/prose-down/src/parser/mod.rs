25a7ba2ac558c5c59e076be0cdb391e8cabd0c07
tett23 <tett23@gmail.com>
2023-03-09 02:41:32 +0900

    Update prose-down parser

--- packages/prose-down/src/parser/mod.rs
+++ packages/prose-down/src/parser/mod.rs
@@ -1,5 +1,5 @@
 use anyhow::Context;
-use pest::iterators::{Pair, Pairs};
+use pest::iterators::Pair;
 use pest::Parser;
 use pest_derive::Parser;
 use prose_ir::Document;
@@ -9,38 +9,19 @@ use prose_ir::Document;
 pub struct ProseParser;
 
 pub fn parse(input: &str) -> anyhow::Result<Document> {
-    ProseParser::parse(Rule::prose, input)
-        .context("parse error")
-        .map(document)
-}
+    let prose = ProseParser::parse(Rule::prose, input).context("parse error")?;
+    let blocks = prose.into_iter().map(block).collect();
 
-fn document(pair: Pairs<Rule>) -> prose_ir::Document {
-    prose_ir::Document {
-        blocks: blocks(pair),
-    }
-}
-
-fn blocks(pairs: Pairs<Rule>) -> Vec<prose_ir::Block> {
-    pairs
-        .into_iter()
-        .filter_map(|b| match b.as_rule() {
-            Rule::EOI => None,
-            _ => Some(block(b.into_inner().next().unwrap())),
-        })
-        .collect()
+    Ok(Document { blocks })
 }
 
 fn block(pair: Pair<Rule>) -> prose_ir::Block {
     match pair.as_rule() {
-        Rule::paragraph => prose_ir::Block::Paragraph(inlines(pair.into_inner())),
+        Rule::paragraph => prose_ir::Block::Paragraph(pair.into_inner().map(inline).collect()),
         Rule::themanticBreak => prose_ir::Block::ThemanticBreak,
         Rule::emptyLine => prose_ir::Block::EmptyLine,
         _ => panic!("{}", pair),
     }
-}
-
-fn inlines(pairs: Pairs<Rule>) -> Vec<prose_ir::Inline> {
-    pairs.into_iter().map(inline).collect()
 }
 
 fn inline(pair: Pair<Rule>) -> prose_ir::Inline {
@@ -48,7 +29,6 @@ fn inline(pair: Pair<Rule>) -> prose_ir::Inline {
         Rule::inlineExpr => prose_ir::Inline::Expr(pair.into_inner().as_str().to_string()),
         Rule::number => prose_ir::Inline::Number(pair.into_inner().as_str().to_string()),
         Rule::text => prose_ir::Inline::Text(pair.as_str().to_string()),
-        Rule::EOI => prose_ir::Inline::Text("".to_string()),
         _ => panic!("{}", pair),
     }
 }
@@ -59,7 +39,7 @@ mod tests {
 
     #[test]
     fn a() {
-        let doc = parse("aa\nb\n---\n\nc\n{a}, ##aa##\n");
+        let doc = parse("aa\nb\n\n---\n\nc\n{a}, ##aa##\n");
         assert!(doc.is_ok());
     }
 }
